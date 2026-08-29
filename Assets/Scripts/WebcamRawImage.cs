dac357224503891324793dc8d589fbd611142fad
zaremahdii <97278325+zaremahdii@users.noreply.github.com>
2026-08-29 22:40:45 +0330

    scene setting

--- Assets/Scripts/WebcamRawImage.cs
+++ Assets/Scripts/WebcamRawImage.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class WebcamRawImage : MonoBehaviour
 {
     [SerializeField] private RawImage cameraPreview;
+    [SerializeField] private AIWebSocketClient webSocketClient;
     [SerializeField] private string deviceName = "";
     [SerializeField] private bool preferFrontFacingCamera = true;
     [SerializeField, Min(1)] private int requestedWidth = 1280;
@@ -14,21 +15,78 @@ public class WebcamRawImage : MonoBehaviour
 
     private WebCamTexture webcamTexture;
     private RectTransform previewRect;
+    private bool isStarting;
+    private bool permissionDenied;
+    private bool wasSocketConnected;
+    private bool receivedFirstFrame;
 
-    private IEnumerator Start()
+    private void Start()
     {
         if (cameraPreview == null)
         {
             Debug.LogError("WebcamRawImage: Assign a RawImage to Camera Preview.", this);
-            yield break;
+            enabled = false;
+            return;
         }
 
         previewRect = cameraPreview.rectTransform;
+        if (webSocketClient == null)
+            webSocketClient = FindObjectOfType<AIWebSocketClient>();
+    }
 
+    private void Update()
+    {
+        if (webSocketClient == null)
+            webSocketClient = FindObjectOfType<AIWebSocketClient>();
+
+        bool socketConnected = webSocketClient != null && webSocketClient.IsConnected;
+        if (socketConnected && !wasSocketConnected)
+            Debug.Log("WebcamRawImage: WebSocket connected. Starting Unity camera preview.", this);
+
+        if (!socketConnected && wasSocketConnected)
+            Debug.Log("WebcamRawImage: WebSocket disconnected. Stopping Unity camera preview.", this);
+
+        wasSocketConnected = socketConnected;
+        if (!socketConnected)
+        {
+            StopCamera();
+            return;
+        }
+
+        if (webcamTexture == null && !isStarting && !permissionDenied)
+            StartCoroutine(StartCamera());
+
+        if (webcamTexture == null || !webcamTexture.isPlaying)
+            return;
+
+        if (webcamTexture.didUpdateThisFrame && !receivedFirstFrame)
+        {
+            receivedFirstFrame = true;
+            Debug.Log($"WebcamRawImage: First frame received at {webcamTexture.width}x{webcamTexture.height}.", this);
+        }
+
+        previewRect.localEulerAngles = new Vector3(0f, 0f, -webcamTexture.videoRotationAngle);
+        float yScale = webcamTexture.videoVerticallyMirrored ? -1f : 1f;
+        float xScale = mirrorPreview ? -1f : 1f;
+        previewRect.localScale = new Vector3(xScale, yScale, 1f);
+    }
+
+    private IEnumerator StartCamera()
+    {
+        isStarting = true;
+        Debug.Log("WebcamRawImage: Requesting camera permission.", this);
         yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
         if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
         {
+            permissionDenied = true;
+            isStarting = false;
             Debug.LogError("WebcamRawImage: Camera permission was not granted.", this);
+            yield break;
+        }
+
+        if (webSocketClient == null || !webSocketClient.IsConnected)
+        {
+            isStarting = false;
             yield break;
         }
 
@@ -36,10 +94,12 @@ public class WebcamRawImage : MonoBehaviour
         if (devices.Length == 0)
         {
             Debug.LogError("WebcamRawImage: No camera device was found.", this);
+            isStarting = false;
             yield break;
         }
 
         WebCamDevice selectedDevice = FindDevice(devices);
+        Debug.Log($"WebcamRawImage: Selected camera '{selectedDevice.name}'.", this);
         webcamTexture = new WebCamTexture(
             selectedDevice.name,
             requestedWidth,
@@ -48,6 +108,8 @@ public class WebcamRawImage : MonoBehaviour
 
         cameraPreview.texture = webcamTexture;
         webcamTexture.Play();
+        Debug.Log("WebcamRawImage: Camera preview started. Waiting for first frame.", this);
+        isStarting = false;
     }
 
     private WebCamDevice FindDevice(WebCamDevice[] devices)
@@ -72,21 +134,17 @@ public class WebcamRawImage : MonoBehaviour
         return devices[0];
     }
 
-    private void Update()
+    private void OnDisable()
     {
-        if (webcamTexture == null || !webcamTexture.isPlaying)
-            return;
-
-        previewRect.localEulerAngles = new Vector3(0f, 0f, -webcamTexture.videoRotationAngle);
-        float yScale = webcamTexture.videoVerticallyMirrored ? -1f : 1f;
-        float xScale = mirrorPreview ? -1f : 1f;
-        previewRect.localScale = new Vector3(xScale, yScale, 1f);
+        StopCamera();
     }
 
-    private void OnDisable()
+    private void StopCamera()
     {
         if (webcamTexture == null)
             return;
+
+        Debug.Log("WebcamRawImage: Camera preview stopped.", this);
 
         if (webcamTexture.isPlaying)
             webcamTexture.Stop();
@@ -95,5 +153,6 @@ public class WebcamRawImage : MonoBehaviour
             cameraPreview.texture = null;
 
         webcamTexture = null;
+        receivedFirstFrame = false;
     }
 }
